f96b0941e5e738e9f127689eae587285177ec7d2
Michael Gachunji <mikegachunji@gmail.com>
2018-08-04 17:40:06 +0300

    Added logging in and logging out users

--- project/models.py
+++ project/models.py
@@ -1,5 +1,6 @@
 from project import db
 from datetime import datetime
+from sqlalchemy.ext.hybrid import hybrid_method, hybrid_property
  
  
 class Birth(db.Model):
@@ -41,11 +42,37 @@ class User(db.Model):
     username = db.Column(db.String, unique=True, nullable=False)
     email = db.Column(db.String, unique=True, nullable=False)
     password_plaintext = db.Column(db.String, nullable=False)  # TEMPORARY - TO BE DELETED IN FAVOR OF HASHED PASSWORD
+    authenticated = db.Column(db.Boolean, default=False)
  
     def __init__(self, username, email, password_plaintext):
         self.email = email
         self.username = username
         self.password_plaintext = password_plaintext
+        self.authenticated = False
+ 
+    @hybrid_method
+    def is_correct_password(self, plaintext_password):
+        return self.password_plaintext == plaintext_password
+ 
+    @property
+    def is_authenticated(self):
+        """Return True if the user is authenticated."""
+        return self.authenticated
+ 
+    @property
+    def is_active(self):
+        """Always True, as all users are active."""
+        return True
+ 
+    @property
+    def is_anonymous(self):
+        """Always False, as anonymous users aren't supported."""
+        return False
+ 
+    def get_id(self):
+        """Return the email address to satisfy Flask-Login's requirements."""
+        """Requires use of Python 3"""
+        return str(self.id)
  
     def __repr__(self):
         return '<User {0}>'.format(self.name)
--- project/users/views.py
+++ project/users/views.py
@@ -1,8 +1,9 @@
 from flask import render_template, Blueprint, request, redirect, url_for, flash
 from project import db
+from flask_login import login_user, current_user, login_required, logout_user
 from sqlalchemy.exc import IntegrityError
 from project.models import User
-from project.users.forms import RegisterForm
+from project.users.forms import RegisterForm, LoginForm
 
 
 def flash_errors(form):
@@ -39,3 +40,34 @@ def register():
                 db.session.rollback()
                 flash('ERROR! Email ({}) already exists.'.format(form.email.data), 'error')
     return render_template('register.html', form=form)
+
+
+@users_blueprint.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm(request.form)
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            user = User.query.filter_by(email=form.email.data).first()
+            if user is not None and user.is_correct_password(form.password.data):
+                user.authenticated = True
+                db.session.add(user)
+                db.session.commit()
+                login_user(user)
+                flash('Thanks for logging in, {}'.format(current_user.email))
+                return redirect(url_for('births.index'))
+            else:
+                flash('ERROR! Incorrect login credentials.', 'error')
+    return render_template('login.html', form=form)
+
+
+
+@users_blueprint.route('/logout')
+@login_required
+def logout():
+    user = current_user
+    user.authenticated = False
+    db.session.add(user)
+    db.session.commit()
+    logout_user()
+    flash('Goodbye!', 'info')
+    return redirect(url_for('users.login'))
